1e69018b52752fddbe4087918fab5cad1d1c0e84
Thom van Kalkeren <thom@argu.co>
2021-07-14 13:01:49 +0200

    core#1 Upgrade to ktor 1.6.1
    Fixes an issue where cached dns resolution was used on retries https://ktor.io/changelog/1.6/

--- src/io/ontola/cache/createClient.kt
+++ src/io/ontola/cache/createClient.kt
@@ -1,15 +1,24 @@
 package io.ontola.cache
 
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.engine.mock.*
-import io.ktor.client.features.*
-import io.ktor.client.features.auth.*
-import io.ktor.client.features.json.*
-import io.ktor.client.features.json.serializer.*
-import io.ktor.client.features.logging.*
-import io.ktor.http.*
-import io.ktor.util.*
+import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.respond
+import io.ktor.client.engine.mock.toByteArray
+import io.ktor.client.features.UserAgent
+import io.ktor.client.features.auth.Auth
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logging
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.Url
+import io.ktor.http.fullPath
+import io.ktor.http.headersOf
+import io.ktor.http.hostWithPort
+import io.ktor.util.KtorExperimentalAPI
 import io.ontola.cache.features.TenantFinderRequest
 import io.ontola.cache.features.TenantFinderResponse
 import kotlinx.serialization.decodeFromString
@@ -22,7 +31,7 @@ private val Url.fullUrl: String get() = "${protocol.name}://$hostWithPortIfRequi
 
 @OptIn(KtorExperimentalAPI::class)
 fun createClient(testing: Boolean): HttpClient {
-    val configure: HttpClientConfig<*>.() -> Unit =  {
+    val configure: HttpClientConfig<*>.() -> Unit = {
         install(Auth) {}
         install(JsonFeature) {
             serializer = KotlinxSerializer()
@@ -33,7 +42,9 @@ fun createClient(testing: Boolean): HttpClient {
         install(UserAgent) { agent = "cache" }
     }
 
-    return if (testing) {
+    return if (!testing) {
+        HttpClient(CIO, configure)
+    } else {
         HttpClient(MockEngine) {
             configure()
             engine {
@@ -61,7 +72,5 @@ fun createClient(testing: Boolean): HttpClient {
                 }
             }
         }
-    } else {
-        HttpClient(CIO, configure)
     }
 }
