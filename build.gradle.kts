e401313a928e7b69201647711c237c08afc02f2c
Thom van Kalkeren <thom@argu.co>
2022-01-05 10:52:53 +0100

    core#4 Add Dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ plugins {
 
     kotlin("multiplatform") version "1.6.10"
     kotlin("plugin.serialization") version "1.6.10"
+    id("org.jetbrains.dokka") version "1.6.10"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
