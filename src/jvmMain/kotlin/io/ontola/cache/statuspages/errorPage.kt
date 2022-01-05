894c0c02bd7011e63f8aab7de2fc657ab799f6ac
Arthur Dingemans <arthur@argu.co>
2022-01-05 12:26:49 +0100

    core#902 Add title and body to error pages

--- src/jvmMain/kotlin/io/ontola/cache/statuspages/errorPage.kt
+++ src/jvmMain/kotlin/io/ontola/cache/statuspages/errorPage.kt
@@ -6,14 +6,36 @@ import kotlinx.html.body
 import kotlinx.html.h1
 import kotlinx.html.head
 import kotlinx.html.meta
+import kotlinx.html.p
 import kotlinx.html.title
+
+fun titleForStatus(status: HttpStatusCode): String {
+    return when (status) {
+        HttpStatusCode.BadGateway -> "Bad gateway"
+        HttpStatusCode.Forbidden -> "This item is hidden"
+        HttpStatusCode.NotFound -> "This item is not found"
+        HttpStatusCode.Unauthorized -> "Unauthorized"
+        else -> "Internal server error"
+    }
+}
+
+fun bodyForStatus(status: HttpStatusCode): String {
+    return when (status) {
+        HttpStatusCode.BadGateway -> "There was a networking issue during this request, please retry or try again later."
+        HttpStatusCode.Forbidden -> "Maybe it's visible after logging in."
+        HttpStatusCode.NotFound -> "Maybe the item you are looking for is deleted or never existed."
+        HttpStatusCode.Unauthorized -> "You have to be logged in to view this resource."
+        else -> "An error occurred on our side, please try again later."
+    }
+}
 
 fun HTML.errorPage(status: HttpStatusCode) {
     head {
         meta(charset = "utf-8")
-        title("Error")
+        title(titleForStatus(status))
     }
     body {
-        h1 { +"Error" }
+        h1 { +titleForStatus(status) }
+        p { +bodyForStatus(status) }
     }
 }
