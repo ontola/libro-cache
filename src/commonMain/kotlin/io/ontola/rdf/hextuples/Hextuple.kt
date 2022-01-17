3d50ce26874b13ddd2016e1a9398de8675262330
Thom van Kalkeren <thom@argu.co>
2022-01-17 14:22:07 +0100

    core#12 Add hextuple serializer

--- src/commonMain/kotlin/io/ontola/rdf/hextuples/Hextuple.kt
+++ src/commonMain/kotlin/io/ontola/rdf/hextuples/Hextuple.kt
@@ -1,5 +1,15 @@
 package io.ontola.rdf.hextuples
 
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+@Serializable(with = HextupleSerializer::class)
 data class Hextuple(
     val subject: String,
     val predicate: String,
@@ -25,6 +35,31 @@ data class Hextuple(
             DataType.fromValue(data[HexPosition.DataType.ordinal]),
             data[HexPosition.Language.ordinal],
             data[HexPosition.Graph.ordinal],
+        )
+    }
+}
+
+object HextupleSerializer : KSerializer<Hextuple> {
+    @OptIn(ExperimentalSerializationApi::class)
+    val serializer = ListSerializer(String.serializer())
+    override val descriptor: SerialDescriptor = serializer.descriptor
+
+    override fun serialize(encoder: Encoder, value: Hextuple) {
+        val list = encoder.beginCollection(descriptor, 6)
+        value.toArray().forEachIndexed { index, element ->
+            list.encodeStringElement(descriptor, index, element)
+        }
+        list.endStructure(descriptor)
+    }
+
+    override fun deserialize(decoder: Decoder): Hextuple = serializer.deserialize(decoder).let {
+        Hextuple(
+            subject = it[0],
+            predicate = it[1],
+            value = it[2],
+            datatype = DataType.fromValue(it[3]),
+            language = it[4],
+            graph = it[5],
         )
     }
 }
