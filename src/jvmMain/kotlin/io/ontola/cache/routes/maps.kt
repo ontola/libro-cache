ed517367d72072811791eff06b05961fc6db9891
Arthur Dingemans <arthur@argu.co>
2022-01-18 16:34:45 +0100

    core#978 Fix map access token request

--- src/jvmMain/kotlin/io/ontola/cache/routes/maps.kt
+++ src/jvmMain/kotlin/io/ontola/cache/routes/maps.kt
@@ -3,25 +3,32 @@ package io.ontola.cache.routes
 import io.ktor.client.call.body
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.application.call
-import io.ktor.server.response.respond
+import io.ktor.server.response.respondText
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.get
 import io.ktor.util.AttributeKey
 import io.ontola.cache.plugins.MapsConfig
 import io.ontola.cache.plugins.cacheConfig
 import kotlinx.datetime.Clock
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import kotlin.time.Duration.Companion.hours
 
+@Serializable
 data class AccessTokenRequest(
     val scopes: List<String>,
     val expires: String,
 )
 
+@Serializable
 data class AccessTokenResponse(
-    val accessToken: String,
-    val expiresAt: String,
+    val expiresAt: String? = null,
+    val token: String,
 )
 
 fun Routing.mountMaps() {
@@ -34,7 +41,7 @@ fun Routing.mountMaps() {
 
         val token = call.createMapsToken(mapsConfig)
 
-        call.respond(token)
+        call.respondText(Json.encodeToString(token))
     }
 }
 
@@ -43,6 +50,7 @@ private suspend fun ApplicationCall.createMapsToken(mapsConfig: MapsConfig): Acc
     val expiresAt = Clock.System.now().plus(1.hours).toString()
 
     val response = config.client.post(mapsConfig.tokenEndpoint) {
+        contentType(ContentType.Application.Json)
         setBody(
             AccessTokenRequest(
                 scopes = mapsConfig.scopes,
@@ -51,9 +59,9 @@ private suspend fun ApplicationCall.createMapsToken(mapsConfig: MapsConfig): Acc
         )
     }
 
-    val token = response.body<AccessTokenResponse>()
+    val token = response.body<AccessTokenResponse>().copy(expiresAt = expiresAt)
 
-    if (!token.accessToken.startsWith("tk.")) {
+    if (!token.token.startsWith("tk.")) {
         throw Exception("Map access token not temporary")
     }
 
