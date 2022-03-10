09b5243b4bc527bae933f955c23e315b214c29e6
Arthur Dingemans <arthur@argu.co>
2022-03-10 11:15:03 +0100

    core#1041 Revert service worker

--- src/commonMain/kotlin/io/ontola/apex/webmanifest/ServiceWorker.kt
+++ src/commonMain/kotlin/io/ontola/apex/webmanifest/ServiceWorker.kt
@@ -5,5 +5,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ServiceWorker(
     val src: String = "/f_assets/sw.js",
-    val scope: String = if (src == "/") "/sw.js" else "$src/sw.js",
+    val scope: String = "/",
 )
