d8abc2c309df92eeb68571d89dbd7dfc594392e3
Thom van Kalkeren <thom@argu.co>
2022-04-26 14:40:50 +0200

    core#1160 Convert redirect plugin

--- src/jvmMain/kotlin/io/ontola/cache/plugins/Redirect.kt
+++ src/jvmMain/kotlin/io/ontola/cache/plugins/Redirect.kt
@@ -1,54 +1,30 @@
 package io.ontola.cache.plugins
 
-import io.ktor.server.application.ApplicationCall
-import io.ktor.server.application.ApplicationCallPipeline
-import io.ktor.server.application.ApplicationPlugin
-import io.ktor.server.application.call
 import io.ktor.server.application.createApplicationPlugin
 import io.ktor.server.plugins.origin
 import io.ktor.server.request.path
 import io.ktor.server.response.respondRedirect
-import io.ktor.util.AttributeKey
-import io.ktor.util.pipeline.PipelineContext
 
 private const val redirectPrefix = "redirect"
 
 fun removeTrailingSlash(value: String) = if (value.endsWith('/')) value.dropLast(1) else value
 
-class Redirect(
-    private val storage: Storage,
-) {
-    class Configuration {
-        lateinit var storage: Storage
+class RedirectConfiguration {
+    lateinit var storage: Storage
+
+    fun complete(storage: Storage) {
+        if (!this::storage.isInitialized) this.storage = storage
     }
+}
 
-    private suspend fun intercept(context: PipelineContext<Unit, ApplicationCall>) {
-        val uri = removeTrailingSlash("${context.call.request.origin.host}${context.call.request.path()}")
-        val redirectLocation = storage.getString(redirectPrefix, uri)
+val Redirect = createApplicationPlugin(name = "Redirect", ::RedirectConfiguration) {
+    pluginConfig.complete(application.persistentStorage)
 
-        redirectLocation ?: return context.proceed()
+    onCall { call ->
+        val uri = removeTrailingSlash("${call.request.origin.host}${call.request.path()}")
+        val redirectLocation = pluginConfig.storage.getString(redirectPrefix, uri)
 
-        context.call.respondRedirect(redirectLocation, true)
-        context.finish()
-    }
-
-    companion object Plugin : ApplicationPlugin<ApplicationCallPipeline, Configuration, Redirect> {
-        override val key = AttributeKey<Redirect>("Redirect")
-
-        override fun install(pipeline: ApplicationCallPipeline, configure: Configuration.() -> Unit): Redirect {
-            val config = Configuration()
-                .apply {
-                    storage = pipeline.persistentStorage
-                }.apply(configure)
-            val feature = Redirect(
-                config.storage,
-            )
-
-            pipeline.intercept(ApplicationCallPipeline.Plugins) {
-                feature.intercept(this)
-            }
-
-            return feature
-        }
+        if (redirectLocation != null)
+            call.respondRedirect(redirectLocation, true)
     }
 }
