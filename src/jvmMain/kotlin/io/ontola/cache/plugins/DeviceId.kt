6f1428ff2d44377c02f69431efcabb3470b9d180
Thom van Kalkeren <thom@argu.co>
2022-04-26 13:56:17 +0200

    core#1160 Convert DeviceId plugin

--- src/jvmMain/kotlin/io/ontola/cache/plugins/DeviceId.kt
+++ src/jvmMain/kotlin/io/ontola/cache/plugins/DeviceId.kt
@@ -1,53 +1,36 @@
 package io.ontola.cache.plugins
 
 import io.ktor.server.application.ApplicationCall
-import io.ktor.server.application.ApplicationCallPipeline
-import io.ktor.server.application.ApplicationPlugin
-import io.ktor.server.application.call
+import io.ktor.server.application.createApplicationPlugin
 import io.ktor.server.application.plugin
 import io.ktor.server.request.path
 import io.ktor.server.sessions.get
 import io.ktor.server.sessions.sessions
 import io.ktor.server.sessions.set
 import io.ktor.util.AttributeKey
-import io.ktor.util.pipeline.PipelineContext
 import java.util.UUID
 
-class DeviceId(private val configuration: Configuration) {
-    class Configuration {
-        var blacklist: List<String> = emptyList()
+class DeviceIdConfiguration {
+    var blacklist: List<String> = emptyList()
+}
+
+val DeviceId = createApplicationPlugin(name = "DeviceId", ::DeviceIdConfiguration) {
+
+    fun isBlacklisted(path: String): Boolean {
+        return pluginConfig.blacklist.any { fragment -> path.startsWith(fragment) }
     }
 
-    private fun intercept(context: PipelineContext<Unit, ApplicationCall>) {
-        if (!isBlacklisted(context.call.request.path())) {
-            val deviceId = context.call.sessions.get<String>() ?: generateSessionId(context)
-            context.call.attributes.put(DeviceIdKey, deviceId)
-        }
-    }
-
-    private fun isBlacklisted(path: String): Boolean {
-        return configuration.blacklist.any { fragment -> path.startsWith(fragment) }
-    }
-
-    private fun generateSessionId(context: PipelineContext<Unit, ApplicationCall>): String {
+    fun generateSessionId(call: ApplicationCall): String {
         val id = UUID.randomUUID().toString()
-        context.call.sessions.set(id)
+        call.sessions.set(id)
 
         return id
     }
 
-    companion object Plugin : ApplicationPlugin<ApplicationCallPipeline, Configuration, DeviceId> {
-        override val key = AttributeKey<DeviceId>("DeviceId")
-
-        override fun install(pipeline: ApplicationCallPipeline, configure: Configuration.() -> Unit): DeviceId {
-            val configuration = Configuration().apply(configure)
-            val feature = DeviceId(configuration)
-
-            pipeline.intercept(ApplicationCallPipeline.Plugins) {
-                feature.intercept(this)
-            }
-
-            return feature
+    onCall { call ->
+        if (!isBlacklisted(call.request.path())) {
+            val deviceId = call.sessions.get<String>() ?: generateSessionId(call)
+            call.attributes.put(DeviceIdKey, deviceId)
         }
     }
 }
