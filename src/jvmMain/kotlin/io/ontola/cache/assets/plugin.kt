afa1947dabaecc25f94324a8a308cfa01e7f5245
Thom van Kalkeren <thom@argu.co>
2022-04-26 14:09:32 +0200

    core#1160 Convert assets plugin

--- src/jvmMain/kotlin/io/ontola/cache/assets/plugin.kt
+++ src/jvmMain/kotlin/io/ontola/cache/assets/plugin.kt
@@ -1,36 +1,24 @@
 package io.ontola.cache.assets
 
-import io.ktor.server.application.ApplicationCallPipeline
-import io.ktor.server.application.ApplicationPlugin
-import io.ktor.server.application.plugin
+import io.ktor.server.application.ApplicationCall
+import io.ktor.server.application.createApplicationPlugin
 import io.ktor.util.AttributeKey
-import io.ontola.cache.plugins.CacheSession
 import io.ontola.cache.plugins.cacheConfig
 
-class Assets(private val configuration: Configuration) {
-    class Configuration
+val Assets = createApplicationPlugin(name = "Assets") {
+    val assets = loadAssetsManifests(application.cacheConfig)
 
-    companion object Plugin : ApplicationPlugin<ApplicationCallPipeline, Configuration, Assets> {
-        override val key = AttributeKey<Assets>("Assets")
-
-        override fun install(pipeline: ApplicationCallPipeline, configure: Configuration.() -> Unit): Assets {
-            val configuration = Configuration().apply(configure)
-            val feature = Assets(configuration)
-            val assets = loadAssetsManifests(pipeline.cacheConfig)
-            pipeline.attributes.put(AssetsKey, assets)
-
-            return feature
-        }
+    onCall { call ->
+        call.attributes.put(AssetsKey, assets)
     }
 }
 
 private val AssetsKey = AttributeKey<AssetsManifests>("AssetsKey")
 
-internal val ApplicationCallPipeline.assets: AssetsManifests
+internal val ApplicationCall.assets: AssetsManifests
     get() = attributes.getOrNull(AssetsKey) ?: reportMissingAssets()
 
-private fun ApplicationCallPipeline.reportMissingAssets(): Nothing {
-    plugin(CacheSession) // ensure the feature is installed
+private fun reportMissingAssets(): Nothing {
     throw AssetsNotYetConfiguredException()
 }
 
